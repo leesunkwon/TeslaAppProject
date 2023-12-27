3c2fd909035f5853edc026787a85db9d07c52747
스윗남 (Kotlinsun) <68762235+leesunkwon@users.noreply.github.com>
2023-12-27 23:40:39 +0900

    환경 세팅

--- app/src/main/java/com/kotlinsun/teslaappproject/MainActivity.kt
+++ app/src/main/java/com/kotlinsun/teslaappproject/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kotlinsun.teslaappproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlinsun.teslaappproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
